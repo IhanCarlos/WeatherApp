4c246e04dd4407221bfdd94e33ee0a97ec0e68cf
ihan <ihancarlosihan@gmail.com>
2022-10-20 13:23:33 -0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by ihan carlos on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
